8b148f1715dc29a17f9966be1aeee050a1ad4870
Chris DiStasio <stahzz1970@gmail.com>
2024-04-30 13:24:11 -0500

    Solves 2

--- solution2.py
+++ solution2.py
@@ -1,1 +1,12 @@
 # Write code for algorithm 2 below
+
+def natural_numbers(x, n):
+    if x > n :
+        return
+    else: 
+        print(x)
+        natural_numbers(x+1, n)
+
+n=3
+natural_numbers(1, n)
+
